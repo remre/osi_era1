30cdfcb83ce8c0162ff23a0bfc561102ad690380
remre <basarremre@outlook.com>
2025-01-05 14:27:14 +0300

    feat: add frontend folder with Prettier, ESLint, and TailwindCSS configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="flex items-center justify-center mx-auto min-h-screen">
+        event app
+      </div>
+    </div>
+  );
+}
+
+export default App;
